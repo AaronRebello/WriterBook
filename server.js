a0211a74d972ae49b1633821538cf4f05b390d75
unknown <rebelloaaron4@gmail.com>
2020-08-21 21:55:16 +0530

    new comment

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const {
 
 const app = express();
 
-const PORT = process.env || 5000;
+const PORT = process.env.PORT || 5000;
 
 // ******* in Terminal to run server - npm run server , node server.js **********
 
